d9525f9406663b9c242e2fa27465b3864724355c
Manue <emanuelblaj93@gmail.com>
2020-04-29 16:57:03 +0300

    Demonstrate thread priorities

---
+++ src/com/threadimplrunnable/Priority.java
@@ -0,0 +1,33 @@
+package com.threadimplrunnable;
+
+// Demonstrate thread priorities.
+
+public class Priority implements Runnable {
+	int count;
+	Thread thrd;
+	
+	static boolean stop = false;
+	static String currentName;
+	
+	public Priority(String name) {
+		thrd = new Thread(this, name);
+		count = 0;
+		currentName = name;
+		
+	}
+
+//	Entry point of thread.
+	public void run() {
+		System.out.println(thrd.getName() + " starting");
+		do {
+			count++;
+			if(currentName.compareTo(thrd.getName()) != 0) {
+				currentName = thrd.getName();
+				System.out.println("In " + currentName);
+			}
+		} while(stop == false && count < 10000000);
+		stop = true;
+		
+		System.out.println("\n" + thrd.getName() + " terminating.");
+	}
+}
