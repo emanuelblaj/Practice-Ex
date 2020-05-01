172be0a838a16bdcdf5130ca52507a9c68fd208a
Manue <emanuelblaj93@gmail.com>
2020-05-01 15:52:32 +0300

    Create a traffic lights program that shows red, yellow and green color

---
+++ src/com/trafficlightdemonstration/TrafficLightColor.java
@@ -0,0 +1,9 @@
+package com.trafficlightdemonstration;
+
+/*
+ * A simulation of a traffic light that uses
+ * an enumeration to describe the light's color.
+ */
+public enum TrafficLightColor {
+	RED, GREEN, YELLOW
+}
---
+++ src/com/trafficlightdemonstration/TrafficLightDemo.java
@@ -0,0 +1,17 @@
+ package com.trafficlightdemonstration;
+
+public class TrafficLightDemo {
+
+	public static void main(String[] args) {
+		TrafficLightSimulator tl = new TrafficLightSimulator(TrafficLightColor.GREEN);
+		Thread thrd = new Thread(tl);
+		thrd.start();
+		
+		for(int i = 0; i < 9; i++) {
+			System.out.println(tl.getColor());
+			tl.waitForChange();
+		}
+		tl.cancel();
+	}
+
+}
