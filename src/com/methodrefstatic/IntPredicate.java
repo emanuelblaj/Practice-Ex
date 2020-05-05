82d2eb3083ecde67eff98cd0dd9a0f8db14b2dd0
Manue <emanuelblaj93@gmail.com>
2020-05-05 13:52:03 +0300

    Method references to static methods example

---
+++ src/com/methodrefstatic/IntPredicate.java
@@ -0,0 +1,9 @@
+package com.methodrefstatic;
+
+// Demonstrate a method referecnce for a static method.
+
+// A functional interface for numeric predicates that operate on integer values.
+
+public interface IntPredicate {
+	boolean test(int n);
+}
